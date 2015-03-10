4f5aba2091ae9f35316dadfb1d2d992c1e2dddbe
Victor Hernandez Perez <alu0100697032.edu.es>
2015-03-10 18:15:18 +0000

    Added some animation with jQuery

---
+++ js/animaciones.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+   $("#title").animate({
+      bottom: '100px',
+      fontSize: '75px',
+   }, "slow");
+   $("#pl").animate({
+      bottom: '100px',
+   }, "slow");
+   $("#opt").animate({
+      bottom: '100px',
+   }, "slow");
+   $("#daa").animate({
+      bottom: '100px',
+   }, "slow");
+   $("#navbar").animate({
+      left: '100px',
+   }, "slow");
+});
